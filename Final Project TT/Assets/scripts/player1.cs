11a4aacb4c6b6097e67faf487ffd8565e25fb47b
BCrawf521 <166097@student.asd20.org>
2018-12-05 14:00:55 -0700

    Put commented code in Visual Studio and made our first map for our tanks.

--- Final Project TT/Assets/scripts/player1.cs
+++ Final Project TT/Assets/scripts/player1.cs
@@ -20,19 +20,19 @@ public class player1 : MonoBehaviour
     {
 
         velocity = rbody.velocity;
-        if (Input.GetKey(KeyCode.W))
+        if (Input.GetKey(KeyCode.E))
         {
             velocity += Vector3.up * Time.deltaTime * speed;
         }
-        if (Input.GetKey(KeyCode.S))
+        if (Input.GetKey(KeyCode.D))
         {
             velocity -= Vector3.up * Time.deltaTime * speed;
         }
-        if (Input.GetKey(KeyCode.A))
+        if (Input.GetKey(KeyCode.F))
         {
             velocity -= Vector3.right * Time.deltaTime * speed;
         }
-        if (Input.GetKey(KeyCode.D))
+        if (Input.GetKey(KeyCode.S))
         {
             velocity += Vector3.right * Time.deltaTime * speed;
         }
@@ -45,3 +45,100 @@ public class player1 : MonoBehaviour
 
     }
 }
+
+//public class Square : MonoBehaviour
+//{
+
+//    [SerializeField]
+//    GameObject ballPrefab;
+//    float speed = 1.5f;
+
+//    Vector3 velocity = new Vector3();
+
+//    // Use this for initialization
+//    void Start()
+//    {
+//
+//    }
+
+//    // Update is called once per frame
+//    void Update()
+//    {
+//        velocity = new Vector3(0, 0, 0);
+
+//        if (Input.GetKey(KeyCode.S))
+//        {
+//            transform.eulerAngles = new Vector3(0, 0, transform.eulerAngles.z + 200 * Time.deltaTime);
+//        }
+//        if (Input.GetKey(KeyCode.F))
+//        {
+//            transform.eulerAngles = new Vector3(0, 0, transform.eulerAngles.z - 200 * Time.deltaTime);
+//        }
+//        if (Input.GetKey(KeyCode.E))
+//        {
+//            velocity += LookAtDirection(transform.eulerAngles.z);
+//        }
+//        if (Input.GetKey(KeyCode.D))
+//        {
+//            velocity -= LookAtDirection(transform.eulerAngles.z);
+//        }
+
+//        if (Input.GetKeyDown(KeyCode.G))
+//        {
+//            GameManager.Instance.MyCharacter = this;
+//        }
+
+//        velocity.Normalize();
+
+//        ShootBalls();
+//    }
+
+//    void MoveWASD()
+//    {
+//        if (Input.GetKey(KeyCode.W))
+//        {
+//            if (transform.position.y < 1)
+//            {
+//                velocity += Vector3.up;
+//            }
+//        }
+//        if (Input.GetKey(KeyCode.S))
+//        {
+//            if (transform.position.y > -1)
+//            {
+//                velocity += Vector3.down;
+//            }
+//        }
+//        if (Input.GetKey(KeyCode.A))
+//        {
+//            if (transform.position.x > -1.75)
+//            {
+//                velocity += Vector3.left;
+//            }
+//        }
+//        if (Input.GetKey(KeyCode.D))
+//        {
+//            if (transform.position.x < 1.75)
+//            {
+//                velocity += Vector3.right;
+//            }
+//        }
+//    }
+
+//    public Vector3 LookAtDirection(float eulerAnglesZ)
+//    {
+//        return new Vector3(Mathf.Cos(eulerAnglesZ * Mathf.Deg2Rad), Mathf.Sin(eulerAnglesZ * Mathf.Deg2Rad), 0);
+//    }
+
+//    void ShootBalls()
+//    {
+//        transform.position += velocity * Time.deltaTime * speed;
+
+//        if (Input.GetKeyDown(KeyCode.Space))
+//        {
+//            GameObject newBall = Instantiate(ballPrefab);
+//            newBall.transform.position = transform.position;
+//            newBall.GetComponent<Ball>().velocity = LookAtDirection(transform.eulerAngles.z);
+//        }
+//    }
+//}
