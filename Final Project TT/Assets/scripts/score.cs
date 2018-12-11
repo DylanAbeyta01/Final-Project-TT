591bfdf6213a635ead6b33d4e44e34db48d3e769
DylanAbeyta <dylanabeyta@gmail.com>
2018-12-11 13:40:30 -0700

    working on building the score

--- Final Project TT/Assets/scripts/score.cs
+++ Final Project TT/Assets/scripts/score.cs
@@ -2,15 +2,20 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class score : MonoBehaviour {
+public class score : MonoBehaviour
+{
+    [SerializeField]
+    int scoretext = 0;
 
-	// Use this for initialization
-	void Start () {
+    // Use this for initialization
+    void Start ()
+    {
 		
 	}
 	
 	// Update is called once per frame
-	void Update () {
+	void Update ()
+    {
 		
 	}
 }
